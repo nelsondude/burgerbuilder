c87f32c2e565ba23785995c09f91c21feefc1d8c
nelsondude <alexn1336@gmail.com>
2018-05-22 22:51:32 -0400

    skip tests

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import burgerLogo from 'assets/images/burger-logo.png';
 import classes from './Logo.css';
-import { Link } from 'react-router-dom';
+import { Link, Router, NavLink } from 'react-router-dom';
 
 const logo = (props) => (
   <div className={classes.Logo} style={{height: props.height}}>
-    <Link to={'/'}>
-      <img src={burgerLogo} alt=""/>
-    </Link>
+      <Link to={'/'}>
+        <img src={burgerLogo} alt=""/>
+      </Link>
   </div>
 );
 
